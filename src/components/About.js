bbc449ff4e39fa701302bf91a65c3f498b4b214a
Bidyut kundu <106534797+bidyut10@users.noreply.github.com>
2022-08-24 17:41:01 +0530

    final

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+const About = () => {
+  return (
+    <div>
+      <h5>React has been designed from the start for gradual adoption, and you can use as little or as much React as you need. Perhaps you only want to add some “sprinkles of interactivity” to an existing page. React components are a great way to do that.
+
+        The majority of websites aren’t, and don’t need to be, single-page apps. With a few lines of code and no build tooling, try React in a small part of your website. You can then either gradually expand its presence, or keep it contained to a few dynamic widgets.</h5>
+      <Link to="/">Go Back</Link>
+    </div>
+  );
+};
+
+export default About;
